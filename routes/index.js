b682be393f0d0be630486ec43323b95cacc0307d
rifky1211 <rifky363@gmail.com>
2021-06-24 13:39:51 +0700

    challange 22 is done

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 var express = require("express");
 var router = express.Router();
 var moment = require("moment"); // require
@@ -8,49 +9,113 @@ module.exports = function (use) {
   const db = use.db(dbName);
   const page = 1;
   router.get("/", function (req, res, next) {
-      // db.collection('c21').find().count()
-      // .then(
-      //   res => console.log(`${res.result.n}`),
-      //   err => console.log(err),
-      // )
-  var page = parseInt(req.query.page) || 1;
-  var size = 2;
-  var skip = (page - 1) * size;
-  let stringdata = req.query.string
-  let integerdata = parseInt(req.query.integer);
-  let floatdata = parseFloat(req.query.float);
-  let start = req.query.start;
-  let end = req.query.end;
-  let booleandata = req.query.boolean;
-  if(page){
-    db.collection("c21")
-    .find()
-    .limit(size)
-    .skip(skip)
-    .toArray((err, result) => {
-      jumlahData = result.length;
-      jumlahHalaman = Math.ceil(jumlahData / 2)
+    const url = req.url == "/" ? "/?page=1" : req.url;
+    var page = parseInt(req.query.page) || 1;
+    var size = 2;
+    var skip = (page - 1) * size;
+    let stringdata = req.query.string;
+    let integerdata = parseInt(req.query.integer);
+    let floatdata = parseFloat(req.query.float);
+    let start = req.query.start;
+    let end = req.query.end;
+    let booleandata = req.query.boolean;
+    let params = [];
+
+    if (stringdata || integerdata || floatdata || start || end || booleandata) {
+      params.push(
+        { stringdata },
+        { integerdata },
+        { floatdata },
+        { start },
+        { end },
+        { booleandata }
+      );
+    }
+
+    if (params.length > 0) {
+      if (stringdata) {
+        var objectString = { stringdata };
+      }
+      if (integerdata) {
+        var objectInteger = { integerdata: `${integerdata}` };
+      }
+      if (floatdata) {
+        var objectFloat = { floatdata: `${floatdata}` };
+      }
+
+      if (booleandata) {
+        var objectBoolean = { booleandata };
+      }
+
+      let find = {
+        ...objectString,
+        ...objectInteger,
+        ...objectFloat,
+        ...objectBoolean,
+      };
+
+      if (start && end) {
+        let gte = new Date(start);
+        let lt = new Date(end);
+        var date = { datedata: { $gte: gte, $lt: lt } };
+        find = {
+          ...objectString,
+          ...objectInteger,
+          ...objectFloat,
+          ...objectBoolean,
+          ...date,
+        };
+      }
+      console.log(find);
       db.collection("c21")
-        .find()
-        .limit(size)
-        .skip(skip)
+        .find(find)
         .toArray((err, result) => {
-          res.render("index", {data: result, moment: moment});
-
+          let jumlahData = result.length;
+          let jumlahHalaman = Math.ceil(jumlahData / 2);
+          db.collection("c21")
+            .find(find)
+            .limit(size)
+            .skip(skip)
+            .toArray((err, result) => {
+              res.render("index", {
+                data: result,
+                moment: moment,
+                url,
+                page,
+                jumlahHalaman,
+                stringdata,
+                integerdata,
+                floatdata,
+                start,
+                end,
+                booleandata,
+              });
+            });
         });
-    });
-  }
-
+    }
     db.collection("c21")
       .find()
       .toArray((err, result) => {
-        jumlahData = result.length;
-        jumlahHalaman = Math.ceil(jumlahData / 2)
+        let jumlahData = result.length;
+        let jumlahHalaman = Math.ceil(jumlahData / 2);
         db.collection("c21")
           .find()
+          .limit(size)
+          .skip(skip)
           .toArray((err, result) => {
-            res.render("index", {data: result, moment: moment});
-
+            res.render("index", {
+              data: result,
+              moment: moment,
+              url,
+              page,
+              jumlahHalaman,
+              stringdata,
+              integerdata,
+              floatdata,
+              start,
+              end,
+              booleandata,
+            });
           });
       });
   });
@@ -60,12 +125,12 @@ module.exports = function (use) {
   });
 
   router.post("/add", (req, res) => {
-    db.getCollection("c21").insertOne(
+    db.collection("c21").insertOne(
       {
         stringdata: `${req.body.string}`,
         integerdata: `${req.body.integer}`,
         floatdata: `${req.body.float}`,
-        datedata: `'${req.body.date}'`,
+        datedata: new Date(`'${req.body.date}'`),
         booleandata: `${req.body.boolean}`,
       },
       res.redirect("/")
@@ -103,7 +168,7 @@ module.exports = function (use) {
           stringdata: `${req.body.string}`,
           integerdata: `${req.body.integer}`,
           floatdata: `${req.body.float}`,
-          datedata: `'${req.body.date}'`,
+          datedata: new Date(`'${req.body.date}'`),
           booleandata: `${req.body.boolean}`,
         },
       },
